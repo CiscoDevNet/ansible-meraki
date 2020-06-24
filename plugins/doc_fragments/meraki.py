eedcf60910164f1171520ddc71fca598e0d4609f
Kevin Breit <kevin.breit@kevinbreit.net>
2020-06-23 20:41:53 -0500

    Fix errors, thought I did these

--- plugins/doc_fragments/meraki.py
+++ plugins/doc_fragments/meraki.py
@@ -3,6 +3,7 @@
 # Copyright: (c) 2018, Kevin Breit (@kbreit) <kevin.breit@kevinbreit.net>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+__metaclass__ = type
 
 class ModuleDocFragment(object):
     # Standard files for documentation fragment
