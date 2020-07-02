ef8b1bf86147a96f2fda24f66a3695f8f58ba9bd
Kevin Breit <kevin.breit@kevinbreit.net>
2020-07-02 12:35:28 -0500

    Fix error handling on 400 errors

--- plugins/module_utils/network/meraki/meraki.py
+++ plugins/module_utils/network/meraki/meraki.py
@@ -363,41 +363,46 @@ class MerakiModule(object):
 
     def _execute_request(self, path, method=None, payload=None, params=None):
         """ Execute query """
-        resp, info = fetch_url(self.module, self.url,
-                               headers=self.headers,
-                               data=payload,
-                               method=self.method,
-                               timeout=self.params['timeout'],
-                               use_proxy=self.params['use_proxy'],
-                               )
-        self.status = info['status']
+        try:
+            resp, info = fetch_url(self.module, self.url,
+                                   headers=self.headers,
+                                   data=payload,
+                                   method=self.method,
+                                   timeout=self.params['timeout'],
+                                   use_proxy=self.params['use_proxy'],
+                                   )
+            self.status = info['status']
 
-        if self.status == 429:
-            self.module.warn("Rate limiter hit, retry {0}".format(self.retry))
-            self.retry += 1
-            if self.retry <= 10:
-                time.sleep(info['retry-after'])
-                self.request(path, method=method, payload=payload, params=params)
-            else:
-                # raise RateLimitException()
-                self.fail_json(msg="Rate limit retries failed for {url}".format(url=self.url))
-        elif self.status == 500:
-            self.retry += 1
-            self.module.warn("Internal server error 500, retry {0}".format(self.retry))
-            if self.retry <= 10:
-                self.retry_time += self.retry * INTERNAL_ERROR_RETRY_MULTIPLIER
-                time.sleep(self.retry_time)
-                self.request(path, method=method, payload=payload, params=params)
-            else:
-                # raise RateLimitException(e)
-                self.fail_json(msg="Rate limit retries failed for {url}".format(url=self.url))
-        elif self.status == 502:
-            self.module.warn("Internal server error 502, retry {0}".format(self.retry))
-        elif self.status == 400:
-            raise HTTPError("")
-        elif self.status >= 400:
-            self.fail_json(msg=self.status, url=self.url)
-            raise HTTPError("")
+            if self.status == 429:
+                self.module.warn("Rate limiter hit, retry {0}".format(self.retry))
+                self.retry += 1
+                if self.retry <= 10:
+                    time.sleep(info['retry-after'])
+                    self.request(path, method=method, payload=payload, params=params)
+                else:
+                    self.fail_json(msg="Rate limit retries failed for {url}".format(url=self.url))
+            elif self.status == 500:
+                self.retry += 1
+                self.module.warn("Internal server error 500, retry {0}".format(self.retry))
+                if self.retry <= 10:
+                    self.retry_time += self.retry * INTERNAL_ERROR_RETRY_MULTIPLIER
+                    time.sleep(self.retry_time)
+                    self.request(path, method=method, payload=payload, params=params)
+                else:
+                    # raise RateLimitException(e)
+                    self.fail_json(msg="Rate limit retries failed for {url}".format(url=self.url))
+            elif self.status == 502:
+                self.module.warn("Internal server error 502, retry {0}".format(self.retry))
+            elif self.status == 400:
+                raise HTTPError("")
+            elif self.status >= 400:
+                self.fail_json(msg=self.status, url=self.url)
+                raise HTTPError("")
+        except HTTPError:
+            try:
+                self.fail_json(msg="HTTP error {0} - {1} - {2}".format(self.status, self.url, json.loads(info['body'])['errors'][0]))
+            except json.decoder.JSONDecodeError:
+                self.fail_json(msg="HTTP error {0} - {1}".format(self.status, self.url))            
         self.retry = 0  # Needs to reset in case of future retries
         return resp, info
 
