314870f85668e3b5bcae11650f1f9423bf15b753
Kevin Breit <kevin.breit@kevinbreit.net>
2020-07-07 14:46:48 -0500

    Fix responses and crash in webhook

--- plugins/modules/meraki_webhook.py
+++ plugins/modules/meraki_webhook.py
@@ -233,6 +233,7 @@ def main():
         nets = meraki.get_nets(org_id=org_id)
         net_id = meraki.get_net_id(net_name=meraki.params['net_name'], data=nets)
     webhook_id = meraki.params['webhook_id']
+    webhooks = None
     if webhook_id is None and meraki.params['name']:
         webhooks = get_all_webhooks(meraki, net_id)
         webhook_id = get_webhook_id(meraki.params['name'], webhooks)
@@ -269,10 +270,6 @@ def main():
             if meraki.status == 200:
                 meraki.result['data'] = response
                 meraki.exit_json(**meraki.result)
-        if webhook_id is None:  # Make sure it is downloaded
-            if webhooks is None:
-                webhooks = get_all_webhooks(meraki, net_id)
-            webhook_id = get_webhook_id(meraki.params['name'], webhooks)
         if webhook_id is None:  # Test to see if it needs to be created
             if meraki.check_mode is True:
                 meraki.result['data'] = payload
