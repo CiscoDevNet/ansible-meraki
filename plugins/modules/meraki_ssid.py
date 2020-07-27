f6540af1dd1d44fa7dbb3288d9f90871d24511cd
Josh VanDeraa <josh.vanderaa+github@networktocode.com>
2020-07-27 15:02:22 -0500

    Adds WPA3 spec to options (#115)
    * Adds WPA3 spec to options

--- plugins/modules/meraki_ssid.py
+++ plugins/modules/meraki_ssid.py
@@ -66,9 +66,9 @@ options:
         type: str
     wpa_encryption_mode:
         description:
-        - Encryption mode within WPA2 specification.
+        - Encryption mode within specification.
         type: str
-        choices: [WPA1 and WPA2, WPA2 only]
+        choices: [WPA1 and WPA2, WPA2 only, WPA3 Transition Mode, WPA3 only]
     splash_page:
         description:
         - Set to enable splash page and specify type of splash.
@@ -468,7 +468,8 @@ def main():
                          auth_mode=dict(type='str', choices=['open', 'psk', 'open-with-radius', '8021x-meraki', '8021x-radius']),
                          encryption_mode=dict(type='str', choices=['wpa', 'eap', 'wpa-eap']),
                          psk=dict(type='str', no_log=True),
-                         wpa_encryption_mode=dict(type='str', choices=['WPA1 and WPA2', 'WPA2 only']),
+                         wpa_encryption_mode=dict(type='str', choices=[
+                                                  'WPA1 and WPA2', 'WPA2 only', 'WPA3 Transition Mode', 'WPA3 only', ]),
                          splash_page=dict(type='str', choices=['None',
                                                                'Click-through splash page',
                                                                'Billing',
