86d1a51c92bcbc02cf4924b94d88fc27df7f83b4
Kevin Breit <kevin.breit@kevinbreit.net>
2020-07-28 21:55:08 -0500

    Change version_added to 1.0

--- plugins/modules/meraki_admin.py
+++ plugins/modules/meraki_admin.py
@@ -17,7 +17,7 @@ DOCUMENTATION = r'''
 ---
 module: meraki_admin
 short_description: Manage administrators in the Meraki cloud
-version_added: '2.6'
+version_added: '1.0.0'
 description:
 - Allows for creation, management, and visibility into administrators within Meraki.
 options:
