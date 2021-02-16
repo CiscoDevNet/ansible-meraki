0a9923b656997b40b37a8421f7a61a65d1d9b191
Kevin Breit <kevin.breit@kevinbreit.net>
2021-02-16 10:27:50 -0600

    Fix sanity check for whitespace

--- plugins/modules/meraki_mx_content_filtering.py
+++ plugins/modules/meraki_mx_content_filtering.py
@@ -157,6 +157,7 @@ from ansible.module_utils.basic import AnsibleModule, json
 from ansible_collections.cisco.meraki.plugins.module_utils.network.meraki.meraki import MerakiModule, meraki_argument_spec
 from copy import deepcopy
 
+
 def get_category_dict(meraki, full_list, category):
     for i in full_list['categories']:
         if i['name'] == category:
