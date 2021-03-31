e73b0d6b1961f1834795eaf0ed6c4aeaec85626c
Jeff Kala <jeff.kala@networktocode.com>
2021-03-31 14:40:12 -0500

    fix payload for sticky options

--- plugins/modules/meraki_ms_switchport.py
+++ plugins/modules/meraki_ms_switchport.py
@@ -416,8 +416,8 @@ def main():
                         # Need to exit allow_list must be greater than or equal to list_limit.
                         meraki.fail_json(msg='Stick MAC Allow List Limit must be equal to or greater than length of Sticky MAC Allow List.')
                     else:
-                        payload['sticky_mac_allow_list_limit'] == meraki.params["sticky_mac_allow_list_limit"]
-                        payload['sticky_mac_allow_list'] == meraki.params["sticky_mac_allow_list"]
+                        payload['sticky_mac_allow_list_limit'] = meraki.params["sticky_mac_allow_list_limit"]
+                        payload['sticky_mac_allow_list'] = meraki.params["sticky_mac_allow_list"]
                 else:
                     # Need to exit sticky_mac_allow_list requires a limit.
                     meraki.fail_json(msg='Stick MAC Allow List requires a limit be specified. Please use sticky_mac_allow_list_limit.')
