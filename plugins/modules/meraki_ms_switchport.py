baa10931a3e9efb31af7a811fb86c2f533b3ccc5
Jeff Kala <jeff.l.kala@gmail.com>
2021-05-25 21:43:24 -0500

    fix pep8

--- plugins/modules/meraki_ms_switchport.py
+++ plugins/modules/meraki_ms_switchport.py
@@ -536,7 +536,7 @@ def main():
         original = meraki.request(query_path, method='GET')
         # Check voiceVlan to see if state is absent to remove the vlan.
         if meraki.params.get('voice_vlan'):
-            if meraki.params.get('voice_vlan')['state'] == 'absent': 
+            if meraki.params.get('voice_vlan')['state'] == 'absent':
                 payload['voiceVlan'] = None
             else:
                 payload['voiceVlan'] = meraki.params.get('voice_vlan')['vlan']
